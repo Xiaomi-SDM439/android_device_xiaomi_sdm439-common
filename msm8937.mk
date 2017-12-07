f23ba1c5b733d4b9649d9b013cbc80a6da4def19
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 22:15:14 +0800

    sdm439-common: Build dummy android.hidl.[base,manager]@1.0
     * Required by Oreo-MR0 blobs
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- msm8937.mk
+++ msm8937.mk
@@ -212,6 +212,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf \
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
+
 # Healthd
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \

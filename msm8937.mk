6367e34287dc9e6be6c8e690ee040b8199fd2eea
Louis Popi <theh2o64@gmail.com>
2019-12-14 11:30:55 +0100

    sdm439-common: gpio-keys.kl was moved to device specific

--- msm8937.mk
+++ msm8937.mk
@@ -218,7 +218,6 @@ PRODUCT_PACKAGES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 

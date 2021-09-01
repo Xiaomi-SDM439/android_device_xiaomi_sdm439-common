a97ca579131a8b8b41264419f78a09949bb97d3d
EngMoPro <fightstart20000@yahoo.com>
2021-09-01 18:56:50 +0200

    import custom light hal

--- msm8937.mk
+++ msm8937.mk
@@ -229,9 +229,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8937
+    android.hardware.lights-service.xiaomi_sdm439
+
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \

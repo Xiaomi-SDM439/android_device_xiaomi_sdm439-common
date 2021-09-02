d1437dd9a4557cd8d86c703a4d9d3eb60ec2d26e
EngMoPro <fightstart20000@yahoo.com>
2021-09-02 15:54:42 +0200

    upgrade health hal to 2.1

--- msm8937.mk
+++ msm8937.mk
@@ -212,11 +212,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
-
-# Healthd
+    
+# Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # IPACM
 PRODUCT_PACKAGES += \

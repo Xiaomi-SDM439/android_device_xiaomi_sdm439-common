81be397113c5ff0ac9bb3abf0bd11daa77eb040c
EngMoPro <fightstart20000@yahoo.com>
2021-09-08 21:22:14 +0200

    add inputs path

--- msm8937.mk
+++ msm8937.mk
@@ -223,6 +223,12 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
+    
+    
+#keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl    
 
 # FM
 PRODUCT_PACKAGES += \
